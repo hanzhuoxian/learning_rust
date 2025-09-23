73d84700387f8866e0ef97ca38867bb3b9142be1
Jian Han <hanjian@umu.com>
2025-09-23 20:32:17 +0800

    error

--- code/lifetime_fn_new/src/main.rs
+++ code/lifetime_fn_new/src/main.rs
@@ -2,6 +2,5 @@ fn main() {
 }
 
 fn longest<'a>(x: &'a str, y: &str) -> &'a str {
-    let result = String::from("hello");
-    &result
+    x
 }
--- code/macro_attr/src/lib.rs
+++
@@ -1,7 +0,0 @@
-use proc_macro;
-
-#[some_attribute]
-pub fn some_name(input: TokenStream) -> TokenStream {
-    
-}
-
